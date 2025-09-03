66ad2161586e0b9fb6d22327b9051276405ab402
ksiomng <nadana0929@gmail.com>
2025-09-03 18:05:46 +0900

    Fix: 에러처리 추가

--- Class/Class/Data/Network/NetworkManager.swift
+++ Class/Class/Data/Network/NetworkManager.swift
@@ -14,14 +14,14 @@ final class NetworkManager {
     
     private init() { }
     
-    func callRequest<T: Decodable>(api: NetworkRouter, type: T.Type, success: @escaping (T) -> Void) {
+    func callRequest<T: Decodable>(api: NetworkRouter, handler: @escaping ((Result<T, Error>) -> Void)) {
         AF.request(api.endPoint, method: api.method, parameters: api.parameters, headers: api.headers)
             .responseDecodable(of: T.self) { response in
             switch response.result {
             case .success(let value):
-                success(value)
+                handler(.success(value))
             case .failure(let error):
-                print(error)
+                handler(.failure(error))
             }
         }
     }
