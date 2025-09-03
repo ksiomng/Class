8e95c6d0cfd47da2108fa8eb29fea67947e328bc
ksiomng <nadana0929@gmail.com>
2025-09-03 17:36:01 +0900

    Feat: UserDefault에 토큰 정보 저장

---
+++ Class/Class/Helpers/UserDefaultsHelper.swift
@@ -0,0 +1,28 @@
+//
+//  UserDefaultsHelper.swift
+//  Class
+//
+//  Created by Song Kim on 9/3/25.
+//
+
+import UIKit
+
+final class UserDefaultsHelper {
+    static let shared = UserDefaultsHelper()
+    private let defaults = UserDefaults.standard
+    
+    private init() { }
+    
+    var token: String? {
+        get {
+            return defaults.string(forKey: "token")
+        }
+        set {
+            return defaults.set(newValue, forKey: "token")
+        }
+    }
+    
+    func checkLogin() -> Bool {
+        return token != nil
+    }
+}
