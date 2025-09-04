bfd1317797500af856467a6c3fd2b033841253bd
ksiomng <nadana0929@gmail.com>
2025-09-04 23:46:32 +0900

    Refactor: 굳이 함수로 뺄 필요가 없을꺼같아서 수정

--- Class/Class/Custom/RootViewControllerManager.swift
+++ Class/Class/Custom/RootViewControllerManager.swift
@@ -12,27 +12,23 @@ final class RootViewControllerManager {
 
     static func getRootViewController() -> UIViewController {
         if UserDefaultsHelper.shared.checkLogin() {
-            return makeMainTabBar()
+            let homeVC = UINavigationController(rootViewController: HomeViewController())
+            homeVC.tabBarItem = UITabBarItem(title: "카테고리", image: UIImage(systemName: "house.fill"), tag: 0)
+            
+            let searchVC = UINavigationController(rootViewController: SearchViewController())
+            searchVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 1)
+            
+            let profileVC = UINavigationController(rootViewController: ProfileViewController())
+            profileVC.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "person.crop.circle"), tag: 2)
+            
+            let tabBarController = UITabBarController()
+            tabBarController.viewControllers = [homeVC, searchVC, profileVC]
+            tabBarController.tabBar.backgroundColor = .white
+            tabBarController.tabBar.tintColor = .darkGrayC
+            tabBarController.tabBar.barTintColor = .lightGrayC
+            return tabBarController
         } else {
             return LogInViewController()
         }
-    }
-
-    private static func makeMainTabBar() -> UIViewController {
-        let homeVC = UINavigationController(rootViewController: HomeViewController())
-        homeVC.tabBarItem = UITabBarItem(title: "카테고리", image: UIImage(systemName: "house.fill"), tag: 0)
-        
-        let searchVC = UINavigationController(rootViewController: SearchViewController())
-        searchVC.tabBarItem = UITabBarItem(title: "검색", image: UIImage(systemName: "magnifyingglass"), tag: 1)
-        
-        let profileVC = UINavigationController(rootViewController: ProfileViewController())
-        profileVC.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "person.crop.circle"), tag: 2)
-        
-        let tabBarController = UITabBarController()
-        tabBarController.viewControllers = [homeVC, searchVC, profileVC]
-        tabBarController.tabBar.backgroundColor = .white
-        tabBarController.tabBar.tintColor = .darkGrayC
-        tabBarController.tabBar.barTintColor = .lightGrayC
-        return tabBarController
     }
 }
