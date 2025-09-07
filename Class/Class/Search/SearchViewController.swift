50bed16993dcfbd6286ff4c741cb2c7c5f89fdb1
ksiomng <nadana0929@gmail.com>
2025-09-07 16:36:46 +0900

    Feat:  검색화면 구성

--- Class/Class/Search/SearchViewController.swift
+++ Class/Class/Search/SearchViewController.swift
@@ -6,24 +6,42 @@
 //
 
 import UIKit
+import SnapKit
 
 class SearchViewController: UIViewController {
+    
+    private let searchBar = UISearchBar()
+    
+    let centerLabel = {
+        let label = UILabel()
+        label.font = .mediumBoldFont
+        label.text = "원하는 클래스가 있으신가요?"
+        return label
+    }()
+    
+    let classTableView = {
+        let tableView = UITableView()
+        return tableView
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupUI()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupUI() {
+        view.backgroundColor = .white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: setLeftNavigationTitle(title: "클래스 검색"))
+        
+        view.addSubview(searchBar)
+        searchBar.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide)
+            make.horizontalEdges.equalToSuperview()
+        }
+        
+        view.addSubview(centerLabel)
+        centerLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
     }
-    */
-
 }
